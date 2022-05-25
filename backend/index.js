727f4e486dc55ebbfa68b53b3f55615ba227d2d3
Chabbax <chandurdissanayake0@gmail.com>
2022-05-25 20:33:30 +0530

    added working google maps

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+require("dotenv").config();
 
 const app = express();
 dotenv.config();
@@ -27,10 +28,9 @@ mongoose.connection.on("disconnected", () => {
 });
 
 //middlewares
-app.use(cors())
-app.use(cookieParser())
+app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
-
 
 app.listen(PORT, () => {
   connect();
