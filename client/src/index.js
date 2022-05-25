3955527acbb0ac8a396e354c284fd26dfa99d18f
uabinesh <79247690+uabinesh@users.noreply.github.com>
2022-05-25 22:48:20 +0530

    Minor changes

--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
 import { SearchContextProvider } from "./context/SearchContext";
-import { ChakraProvider, theme } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
       <SearchContextProvider>
-        <ChakraProvider theme={theme}>
           <App />
-        </ChakraProvider>
       </SearchContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
